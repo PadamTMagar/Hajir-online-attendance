b89de6de6238156b484e6e726abe471d0ff2cd40
PadamTMagar <padamtmgr567@gmail.com>
2024-12-15 02:22:04 +0545

    update

--- aetsheader.php
+++ aetsheader.php
@@ -1,16 +1,42 @@
 <div class="header">
         <header>        
         <nav class="navbar">
-            <ul>
-                <li><a href="#"><i class="fa-duotone fa-solid fa-users"></i></a></li>
-                <li><a href="#"><i class="fa-regular fa-calendar-days"></i></a></li>
-                <li><a href="#"><i class="fa-regular fa-clipboard"></i></a></li>
-                <li><a href="#"><i class="fa-solid fa-bell fa-shake"></i></a></li>
+            <ul>  
+                <div class="navlist">
+                     <span id="hovertext">User</span> 
+                    <li><a href="#"><i class="fa-duotone fa-solid fa-users"></i></a></li> 
+                </div>
+
+                <div class="navlist">
+                    <span id="hovertext">Calander</span>
+                    <li><a href="#"><i class="fa-regular fa-calendar-days"></i></a></li>
+                </div>
+
+                <div class="navlist">
+                    <span id="hovertext">Report</span>
+                    <li><a href="report.php"><i class="fa-regular fa-clipboard"></i></a></li>
+                </div>
+
+                <div class="navlist">
+                    <span id="hovertext">Notice</span>
+                    <li><a href="#" id="notice" onclick="display()"><i class="fa-solid fa-bell fa-shake"></i></a></li>
+                </div>
+                
                 <div id="date"></div>
                 <div class="login">Logout</div>
             </ul>
         </nav>
     </div>
+
+
+
+
+
+
+
+
+
+    
 
     <script>
         const today = new Date();
--- notice.php
+++ notice.php
@@ -1,18 +1,18 @@
+<?php
+include("aetsheader.php");
+include("aetssidebar.php");
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>hajir</title>
+    <title>Notice</title>
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css">
 </head>
 <body>
-
-<?php require_once('aetsheader.php'); ?>
-<?php require_once('aetssidebar.php'); ?>   
-
- hello world
-
- 
+        
 </body>
+</html>
