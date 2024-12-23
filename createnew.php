1130106ae3942cd35f5433bb4349c5af28db19b5
PadamTMagar <padamtmgr567@gmail.com>
2024-12-24 01:47:40 +0545

    update

--- createnew.php
+++ createnew.php
@@ -18,43 +18,180 @@
             <span class="user">Add user</span>
         </div>
 
-
-        <div class="user_details">
-            <form action="" name="adduser" method="POST">
-
+        <form action="" name="adduser" method="POST">
+            <div class="user_details">
+       
                 <div class="form_row">
-                    <div class="from_group">
+                    <div class="form_group">
                         <label for="fname" id="fname">First Name:*</label>
-                        <input type="text" name="firstname" id="fistname" placeholder="First Name">
+                        <input type="text" name="firstname" id="fistname" placeholder="First Name" required>
                     </div>
 
-                    <div class="from_group">
+                    <div class="form_group">
                         <label for="mname" id="mname">Mid Name:</label>
                         <input type="text" name="midname" id="midname" placeholder="Mid Name">
                     </div>
 
-                    <div class="from_group">
+                    <div class="form_group">
                         <label for="lname" id="lname">Last Name:</label>
                         <input type="text" name="lastname" id="lastname" placeholder="Last Name">
                     </div>
                 </div>
+            
 
-
+            
                 <div class="form_row">
-                    <div class="from_group">
+                    <div class="form_group">
                         <label for="email" id="email">Email:*</label>
-                        <input type="email" name="emailid" id="emailid" placeholder="Email">
+                        <input type="email" name="emailid" id="emailid" placeholder="Email" required>
                     </div>
-                    
-                    <div class="from_group">
+                        
+                    <div class="form_group">
                         <label for="number" id="num">Phone Number:*</label>
-                        <input type="" name="number" id="number" placeholder="Phone Number">
+                        <input type="" name="number" id="number" placeholder="Phone Number" required>
                     </div>
                 </div>
-            </form>
+            </div>
+            
+
+            <div class="role_permiss">
+                <h1>Role & Permissions</h1>
+
+                <div class="form_row">
                     
-        </div>
+                    <div class="form_group">
+                        <label for="username" id="username">Username:*</label>
+                        <input type="text" name="user" id="user" placeholder="User Name" required>
+                    </div>
+
+                    <div class="form_group">
+                        <label for="password" id="password">Password:*</label>
+                        <input type="password" name="passwd" id="passwd" placeholder="Password" required>
+                    </div>
+
+                    <div class="form_group">
+                        <label for="confirm_pw" id="confirm_pw">Confirm Password:*</label>
+                        <input type="password" name="confirm_pw" id="confirm_pw" placeholder="Confirm Password" required>
+                    </div>
+                    </div>
+                    <div class="form_row">
+                        <div class="form_group">
+                            <label for="role_selection" id="role_selection">Select Role:*</label>
+                            <select name="role_selection" id="roleselection">
+                                <option value="">Teacher</option>
+                                <option value="">Student</option>
+                            </select>
+                        </div>
+                        <div class="form_group">
+                            <label for="class_selection" id="class_selection">Select Class:*</label>
+                            <select name="class_selection" id="classselection">
+                                <option value="">class 1</option>
+                                <option value="">class 2</option>
+                            </select>
+                        </div>
+                    </div>   
+            </div>
+
+            <div class="more_details">
+                <h1>More Information</h1>
+
+                <div class="form_row">
+                    <div class="form_group">
+                        <label for="dateofbirth" id="dateofbirth">Date of Birth:</label>
+                        <input type="date" name="date" id="date" required>
+                    </div>
+
+                    <div class="form_group">
+                            <label for="gender" id="gender">Gender:</label>
+                            <select name="gender" id="gender">
+                                <option value="">Male</option>
+                                <option value="">Female</option>
+                                <option value="">Other</option>
+                            </select>
+                    </div>
+
+                    <div class="form_group">
+                            <label for="marital" id="marital">Marital Status:</label>
+                            <select name="marital" id="marital">
+                                <option value="">Married</option>
+                                <option value="">Unmarried</option>
+                                <option value="">Divorced</option>
+                            </select>
+                    </div>
+
+                    <div class="form_group">
+                        <label for="blood" id="blood">Blood Group:</label>
+                        <input type="text" name="blood" id="blood" placeholder="Blood Group" required>
+                    </div>
+                </div>
+
+                <div class="form_row">
+                    <div class="form_group">
+                        <label for="alter_contact" id="alter_contact">Alternate Contact:</label>
+                        <input type="text" name="alter_contact" id="alter_contact" placeholder="Alternate Contact" required>
+                    </div>
+
+                    <div class="form_group">
+                        <label for="per_address" id="per_address">Permanent Address:</label>
+                        <input type="text" name="per_address" id="per_address" placeholder="Permanent Address" required>
+                    </div>
+
+                    <div class="form_group">
+                        <label for="temp_address" id="temp_address">Temporary Address:</label>
+                        <input type="text" name="temp_address" id="temp_address" placeholder="Temporary Address" required>
+                    </div>
+                </div>
+
+                <div class="form_row">
+                    <div class="form_group">
+                        <label for="father_name" id="father_name">Father's Name:</label>
+                        <input type="text" name="father_name" id="father_name" placeholder="Father's Name" required>
+                    </div>
+
+                    <div class="form_group">
+                        <label for="father_occupaion" id="father_occupaion">Father's Occupation:</label>
+                        <input type="text" name="father_occupaion" id="father_occupaion" placeholder="Father's Occupation" required>
+                    </div>
+
+                    <div class="form_group">
+                        <label for="father_contact" id="father_contact">Father's Contact:</label>
+                        <input type="text" name="father_contact" id="father_contact" placeholder="Father's Contact" required>
+                    </div>
+                </div>
+
+                <div class="form_row">
+                    <div class="form_group">
+                        <label for="mother_name" id="mother_name">Mother's Name:</label>
+                        <input type="text" name="mother_name" id="mother_name" placeholder="Mother's Name" required>
+                    </div>
+
+                    <div class="form_group">
+                        <label for="mother_contact" id="mother_contact">Mother's Contact:</label>
+                        <input type="text" name="mother_contact" id="mother_contact" placeholder="Mother's Contact" required>
+                    </div>
+                </div>
+
+                <div class="form_row">
+                    <div class="form_group">
+                        <label for="guardian_name" id="guardian_name">Guardian's Name:</label>
+                        <input type="text" name="guardian_name" id="guardian_name" placeholder="Guardian's Name" required>
+                    </div>
+
+                    <div class="form_group">
+                        <label for="guardian_contact" id="guardian_contact">Guardian's Contact:</label>
+                        <input type="text" name="guardian_contact" id="guardian_contact" placeholder="Guardian's Contact" required>
+                    </div>
+                </div>
+
+            </div>
+        </form>    
     </div>
+    <div class="form-actions">
+                <button type="submit">Submit</button>
+    </div>
+</div>
+    
+    
 </div>
 
 </body>
