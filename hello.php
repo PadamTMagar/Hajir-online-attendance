8c9cc942ad7d2e7737d74b9d38872537a11bd32b
PadamTMagar <padamtmgr567@gmail.com>
2025-01-21 23:59:37 +0545

    update

--- hello.php
+++ hello.php
@@ -4,11 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hello</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <DIV class="container">
     <h1 style="color: red;">
         Helloworld
     </h1>
+    
+    </DIV>
     
 </body>
 </html>
