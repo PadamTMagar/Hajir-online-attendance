23bfedeff9bd80f2c43d2879911a1a9286f72310
PadamTMagar <padamtmgr567@gmail.com>
2025-01-27 00:45:23 +0545

    update

--- usertable.php
+++ usertable.php
@@ -5,7 +5,7 @@ include("aetsconn.php");
 
 $table = "CREATE TABLE user_db (
                 id INT AUTO_INCREMENT PRIMARY KEY NOT NULL, 
-                User_name VARCHAR(30) NOT NULL, 
+                user VARCHAR(30) NOT NULL, 
                 email VARCHAR(30) NOT NULL, 
                 passwd VARCHAR(255) NOT NULL, 
                 user_role VARCHAR(20) NOT NULL
