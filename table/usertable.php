687463e6ae7699052b53a1adf6996ff545c5ae83
PadamTMagar <padamtmgr567@gmail.com>
2025-02-15 23:59:25 +0545

    update

--- table/usertable.php
+++ table/usertable.php
@@ -1,5 +1,12 @@
 <?php 
 include("../aetsconn.php");
 
-
+$table = "CREATE TABLE userlist(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(50) NOT NULL,
+    midname VARCHAR(50),
+    lastname VARCHAR(50) NOT NULL,
+    emailid VARCHAR(50) NOT NULL UNIQUE,
+    number 
+)";
 ?> 
