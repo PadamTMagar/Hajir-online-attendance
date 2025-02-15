3b632caa301726b8dc5985ea1790e75eef163c03
PadamTMagar <padamtmgr567@gmail.com>
2025-02-15 23:13:26 +0545

    update

--- table/usertable.php
+++ table/usertable.php
@@ -1,25 +1,5 @@
-<?php
-include("aetsconn.php");
+<?php 
+include("../aetsconn.php");
 
 
-
-$table = "CREATE TABLE user_db (
-                id INT AUTO_INCREMENT PRIMARY KEY NOT NULL, 
-                user VARCHAR(30) NOT NULL, 
-                email VARCHAR(30) NOT NULL, 
-                passwd VARCHAR(255) NOT NULL, 
-                user_role VARCHAR(20) NOT NULL
-)";
-
-$result = mysqli_query($conn, $table);
-
-if ($result) {
-    echo "Table created successfully";
-} 
-
-else {
-    echo "Unsuccessful creation of table: " . mysqli_error($conn);
-}
-
-mysqli_close($conn);
-?>
+?> 
