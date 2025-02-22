12b34513b544496e502493cdd2235c70191ac11d
PadamTMagar <padamtmgr567@gmail.com>
2025-02-23 00:40:16 +0545

    update

--- table/usertable.php
+++ table/usertable.php
@@ -10,8 +10,23 @@ $table = "CREATE TABLE userlist(
     phone_number INT(10) NOT NULL,
     profile_pic VARCHAR(255) NOT NULL,
     user VARCHAR(50) NOT NULL UNIQUE,
-    passwd VARCHAR(50) NOT NULL , 
-    
+    passwd VARCHAR(50) NOT NULL ,
+    class_selection 
+    dob DATE NOT NULL,
+    gender ENUM('Male' , 'Female' , 'Other') NOT NULL,
+    marital ENUM('Married' , 'Unmarried' , 'Divorced')  ,
+    blood VARCHAR(10) NOT NULL,
+    alter_contact VARCHAR(20),
+    per_address VARCHAR(255) NOT NULL ,
+    temp_address VARCHAR(255) NOT NULL ,
+    father_name VARCHAR(50) NOT NULL ,
+    father_occupation VARCHAR(50) NOT NULL ,
+    father_contact VARCHAR(20) NOT NULL ,
+    mother_name VARCHAR(50) NOT NULL,
+    mother_contact VARCHAR(20) NOT NULL,
+    guardian_name VARCHAR(20),
+    guardian_contact VARCHAR(20),
+    crated_at TIMESTAMP DEFALUT CURRENT_TIMESTAMP
 
 )";
 ?> 
