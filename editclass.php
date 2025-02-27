b5fb06cae97a0e400c608678967e3a4f05ecde3f
PadamTMagar <padamtmgr567@gmail.com>
2025-02-28 02:33:18 +0545

    update

--- editclass.php
+++ editclass.php
@@ -8,9 +8,37 @@ include("aetsconn.php");
 $error = '';
 $succes_msg = '';
 
+if(isset($_GET['id'])){
+    $id = $_GET['id'];
+    $query = "SELECT * FROM Classroom WHERE id = $id";
+    $result = mysqli_query($conn , $query);
+    $row = mysqli_fetch_assoc($result);
 
+    $classname = $row['class_name'];
+    $classteacher = $row['class_teacher'];
+    $class_size = $row['class_size'];
+}
 
+if (isset($_POST['submit'])) {
+    $classname = $_POST['class_name'];
+    $classteacher = $_POST['class_teacher'];
+    $class_size = $_POST['class_size'];
+
+    if (empty($classname) || empty($classteacher) || empty($class_size)) {
+        $error = "All fields are required!";
+    } else {    
+        $update_query = "UPDATE classroom SET classroom_name='$classname', class_teacher='$classteacher', class_size='$class_size' WHERE id='$id'";
+
+        if (mysqli_query($conn, $update_query)) {
+            $succes_msg = "Classroom updated successfully!";
+        } else {
+            $error = "Error updating classroom: " . mysqli_error($conn);
+        }
+    }
+}
 ?>
+
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -39,12 +67,12 @@ $succes_msg = '';
                             <div class="form_row">
                                 <div class="form_group">
                                     <label for="class_name" id="class_name">Class Name:</label>
-                                    <input type="text" name="class_name" id="class_name" placeholder="Class Name" >
+                                    <input type="text" name="class_name" id="class_name"  value="<?php echo isset($classname) ? $classname : ''; ?>">
                                 </div>
 
                                 <div class="form_group">
                                     <label for="class_teacher" id="class_teacher">Class Teacher:</label>
-                                    <select name="class_teacher" id="class_teacher" >
+                                    <select name="class_teacher" id="class_teacher" value="<?php echo isset($classteacher) ? $classteacher : ''; ?>">
                                         <option value="">Please Select</option>
                                         <option value="hari">hari</option>
                                         <option value="shyam">shyam</option>
@@ -54,12 +82,12 @@ $succes_msg = '';
                                 
                                 <div class="form_group">
                                     <label for="class_size" id="class_size">Size of classroom:</label>
-                                    <input type="text" name="class_size" id="class_size" placeholder="Size of classroom" >
+                                    <input type="text" name="class_size" id="class_size" value="<?php echo isset($class_size) ? $class_size : ''; ?>">
                                 </div>
                             </div>
                             </div>
                             <div class="update_button">
-                                <button class="role_update" type="submit" name="submit">Create</button>
+                                <button class="role_update" type="submit" name="submit">Update</button>
                             </div>
                         </form>
 
