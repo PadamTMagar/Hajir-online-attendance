1aeb4f11b3d83def22c0529c936ff4f3a8f73453
PadamTMagar <padamtmgr567@gmail.com>
2026-02-21 23:29:48 +0545

    update

--- aetsconn.php
+++ aetsconn.php
@@ -1,9 +1,9 @@
 <?php
-    $servername = "192.168.1.68";
+    $servername = "192.168.1.65";
     $username = "root";
     $password = "";
     $dbname = "hajir";  
-    $conn = new mysqli($servername, $username, $password, $dbname, 3306  );
+    $conn = new mysqli($servername, $username, $password, $dbname, port: 3306  );
 
     if ($conn->connect_error) {
         die("Connection failed". $conn->connect_error);
